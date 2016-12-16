49f411818b3cbf9af9e459fc8d89cc0dbb139f20
alph0n5e <antoine.al.robert@gmail.com>
2016-12-17 00:36:41 +0100

    Docker Compose orchestration
    - Add `docker-compose.yml` config
    - Add `Dockerfile` for the Node app
    - Add Nginx configuration template

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/env node
 const http = require('http')
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end('<h1>Hello!</h1><p>This is a test...</p>')
+  response.end('<h1>Hello!</h1><p>This is a new test...</p>')
 })
 
 server.listen(8888)
